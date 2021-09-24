918a974ebcd95eba7398829e319808c957ff5a94
veggieburgers <91333814+veggieburgers@users.noreply.github.com>
2021-09-24 08:58:09 -0700

    Update PEMDAS.py

--- PEMDAS.py
+++ PEMDAS.py
@@ -1,10 +1,10 @@
-print("PEMDAS order in reverse")
+# PEMDAS order in reverse
 print(2-5+3/2*4**(4-2))
 
-print("PEMDAS in correct order")
+# PEMDAS in correct order
 print(4**(4-2)*3/2+2-5)
 
-print("PEMDAS order with calculations separated")
+# PEMDAS order with calculations separated
 print((4-2))
 print(4**2)
 print(16*3)
