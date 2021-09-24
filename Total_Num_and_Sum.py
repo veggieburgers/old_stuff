6157f479cb4f798e43962193ff7d435f289815a7
veggieburgers <91333814+veggieburgers@users.noreply.github.com>
2021-09-24 08:53:03 -0700

    Add files via upload

---
+++ Total_Num_and_Sum.py
@@ -0,0 +1,14 @@
+# Original Title: Total Num and Sum (GA)
+# Alternative 1
+total = 0
+
+for num in range(6):
+    total += num
+print(total)
+
+# Alternative 2
+total_sum = range(6)
+print(sum(total_sum))
+
+# My alternative
+print(sum(range(6)))
