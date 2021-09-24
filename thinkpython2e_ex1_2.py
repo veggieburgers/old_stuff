3e6e23d41d8423bdb154c923eaa6c02890436f4b
veggieburgers <91333814+veggieburgers@users.noreply.github.com>
2021-09-24 08:55:58 -0700

    Update thinkpython2e_ex1_2.py

--- thinkpython2e_ex1_2.py
+++ thinkpython2e_ex1_2.py
@@ -1,15 +1,15 @@
 import datetime
 
-print("How many seconds are there in 42 minutes, 42 seconds?")
+# Assignment 1: How many seconds are there in 42 minutes, 42 seconds?
 print(42 * 60 + 42)
 
-print("How many miles are there in 10 kilometers?")
+# Assignment 2: How many miles are there in 10 kilometers?
 print(10 / 1.61)
 
-print("If you run a 10 kilometer race in 42 minutes, 42 seconds, what is your average pace?")
+# Assignment 3: If you run a 10 kilometer race in 42 minutes, 42 seconds, what is your average pace?
 pace_in_sec = 2562 / 6.2
 print(datetime.timedelta(seconds=pace_in_sec))
 
-print("What is your average speed in miles per hour?")
+# Assignment 4: What is your average speed in miles per hour?
 minutes = 42 + 42/60
 print(6.2/minutes)
