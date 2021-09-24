1691921c7e0506b70e5c7b84569c00e2dd3bbcdc
veggieburgers <91333814+veggieburgers@users.noreply.github.com>
2021-09-24 08:18:32 -0700

    Create Cookie_Counter.py

---
+++ Cookie_Counter.py
@@ -0,0 +1,30 @@
+def errormsg():
+    print("Type in a number (ex. 1, 23, 456).")
+
+
+while True:
+    num_cookies = input("How many cookies do we have?\n")
+    try:
+        num_cookies = int(num_cookies)
+        break
+    except ValueError:
+        errormsg()
+
+while True:
+    num_cookies_potential = input("How many are you going to eat?\n")
+    try:
+        num_cookies_potential = int(num_cookies_potential)
+        break
+    except ValueError:
+        errormsg()
+
+while True:
+    has_eaten = input("Have you eaten them? [Y]es or [N]o.\n")
+    if has_eaten.lower() in ["y", "yes"]:
+        print(f"You have", num_cookies - num_cookies_potential, "cookies left.")
+        break
+    elif has_eaten.lower() in ["n", "no"]:
+        print(f"Wise decision, you still have", num_cookies, "to enjoy later.")
+        break
+    else:
+        print("This is a yes or no question.\n")
