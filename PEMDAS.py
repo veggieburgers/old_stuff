be40db77bcf44a400d3901486994736a941d7cac
veggieburgers <91333814+veggieburgers@users.noreply.github.com>
2021-09-24 08:57:46 -0700

    Add files via upload

---
+++ PEMDAS.py
@@ -0,0 +1,13 @@
+print("PEMDAS order in reverse")
+print(2-5+3/2*4**(4-2))
+
+print("PEMDAS in correct order")
+print(4**(4-2)*3/2+2-5)
+
+print("PEMDAS order with calculations separated")
+print((4-2))
+print(4**2)
+print(16*3)
+print(48/2)
+print(24+2)
+print(26-5)
