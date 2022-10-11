3d251422e303be7b3219f7181f7ed315012ca1b3
Johnny <91333814+veggieburgers@users.noreply.github.com>
2022-10-11 01:40:24 -0700

    Delete Simple_If_If.py

--- Simple_If_If.py
+++
@@ -1,9 +0,0 @@
-# Original Title: Simple If If (messing around with my General Assembly homework).
-question = input("Do you now?\n")
-
-yes = "yes"
-
-if question == yes:
-    print("Hmm")
-if question == "no":
-    print("Oh yeah?")
